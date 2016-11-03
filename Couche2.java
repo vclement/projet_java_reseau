890bc1f18297aa497bb5ae5794a014ab22a73fbe
vincent <vincent@debian.org>
2016-11-03 23:15:04 +0100

    lol

---
+++ Couche2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.nio.*;
+
+private String AddresseSource;
+private String AddresssDest;
+private int protoSup;
+
+public class Couche2{
+}
+
+
+
--- WireShark.java
+++ WireShark.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.file.*;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
@@ -8,22 +9,29 @@ class WireShark {
     public static void main( String[] args) throws IOException {
         FileInputStream ficreader = null;
         List list = new LinkedList();
+        byte[] donnee = Files.readAllBytes(Paths.get(args[0]));
+        StringBuilder magicNumber = new StringBuilder();
         int octet=0, cpt=0;
         try {
             ficreader = new FileInputStream ( args[0]);
             while((octet = ficreader.read()) !=-1){
-                list.add(octet);
+                list.add( String.format("%x",octet));
             }
         } catch( FileNotFoundException exc) {
             System.out.println("Le fichier existe");
             System.exit(1);
         }
-
-        for(int i =1; i<=list.size();i++){
-            System.out.print(String.format("%x ", list.get(i-1)));
-            if(i%4 ==0)
-                System.out.println("");
+        
+        System.out.print("Magic Number: ");
+        for(int i = 1; i<=24 ;i++){
+            System.out.print( list.get(i-1) + " ");
+            magicNumber.append((String)list.get(i-1));
         }
+        System.out.println(donnee);
         ficreader.close();
+    }
+
+    public void check(List list){
+        
     }
 }
